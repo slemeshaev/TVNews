ba8a06a9fd6fdbbbbfaa1b2fbe6325123cb50085
slemeshaev <lemeshaevs@gmail.com>
2020-12-02 10:30:53 +0300

    Designing custom collection view

--- TVNews/TVNews/NewsCell.swift
+++ TVNews/TVNews/NewsCell.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 class NewsCell: UICollectionViewCell {
+    //
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
     
+    @IBOutlet weak var unfocusedConstraint: NSLayoutConstraint!
+    
+    var focusedConstraint: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        focusedConstraint = textLabel.topAnchor.constraint(equalTo: imageView.focusedFrameGuide.bottomAnchor, constant: 15)
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        
+        focusedConstraint.isActive = isFocused
+        unfocusedConstraint.isActive = !isFocused
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        super.didUpdateFocus(in: context, with: coordinator)
+        
+        coordinator.addCoordinatedAnimations({
+            self.layoutIfNeeded()
+        })
+
+    }
 }
--- TVNews/TVNews/ViewController.swift
+++ TVNews/TVNews/ViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        return cell
     }
 
 
