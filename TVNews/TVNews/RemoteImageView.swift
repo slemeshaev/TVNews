2da2c32e6d54c5f7cfe618885f982f404eef51be
slemeshaev <lemeshaevs@gmail.com>
2020-12-02 11:45:23 +0300

    RemoteImageview

---
+++ TVNews/TVNews/RemoteImageView.swift
@@ -0,0 +1,20 @@
+//
+//  RemoteImageView.swift
+//  TVNews
+//
+//  Created by Станислав Лемешаев on 02.12.2020.
+//
+
+import UIKit
+
+class RemoteImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
